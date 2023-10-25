435bb345dd6c7c0d8deaf326c28a3fc752a2c338
Oussama  Zahid <ozahid-@e3r3p7.1337.ma>
2023-10-26 00:52:22 +0100

    Chore: setuping the project adding nav and sidebar in layout

---
+++ components/MyContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyContainer = () => {
+  return (
+    <div>myContainer</div>
+  )
+}
+
+export default MyContainer
---
+++ components/index.js
@@ -0,0 +1,10 @@
+
+import MyContainer from "./MyContainer";
+import NavBar from "./NavBar";
+import SideBar from "./SideBar";
+
+export {
+    NavBar,
+    SideBar,
+    MyContainer
+}
