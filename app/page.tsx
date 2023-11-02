367a1caf64542322df622739e40d4a9c2c7b8c41
Oussama  Zahid <ozahid-@e3r10p12.1337.ma>
2023-11-02 02:42:03 +0100

    Chore: min version of landing page is almost done

--- app/page.tsx
+++ app/page.tsx
@@ -7,33 +7,81 @@ import { landing_page_description } from "@/const";
 
 export default function Home() {
     return (
-        <main className="h-full w-full relative xl:w-[90%]">
-            <section className="w-full h-[16%] dred flex flex-col sm:flex-row justify-around sm:justify-between items-center relative px-2 sm:px-10">
+        <div className="h-full w-full relative xl:w-[90%]">
+            <section className="w-full z-10 h-[16%] flex flex-col sm:flex-row justify-around sm:justify-between items-center relative px-2 sm:px-10">
                 <Image
                     src="/youponglogo.png"
                     alt="logo"
                     height={120}
                     width={120}
-                    className="object-contain aspect-square h-[40%] s:h-[60%] lg:h-[100%] w-[20%] max-w-[120px] dred drop-shadow-lg"
+                    className="object-contain aspect-square h-[40%] sm:h-[60%] lg:h-[100%] w-[20%] max-w-[120px] dred drop-shadow-lg"
                 />
                 <div className="dblue sm:w-[60%] max-w-[540px] sm:h-[60%] w-full flex items-left justify-evenly items-center">
                     <Link
                         href="signin"
-                        className="text-white dgreen text-2xl s:text-4xl drop-shadow-lg block font-body font-bold"
+                        className="text-white mt-1 sm:mt-2  text-2xl s:text-4xl drop-shadow-lg block font-body font-bold"
                     >
                         Sign In
+                        <hr className="border-2 border-palette-orange rounded-sm mt-1 sm:mt-2" />
                     </Link>
                     <Link
                         href="signup"
-                        className="text-white sm:hidden dgreen text-2xl s:text-4xl drop-shadow-lg block font-body font-bold"
+                        className="text-white sm:hidden dgreen text-2xl mt-1 sm:mt-2 s:text-4xl drop-shadow-lg block font-body font-bold"
                     >
                         Sign Up
+                        <hr className="border-2 border-palette-orange rounded-sm mt-1 sm:mt-2" />
                     </Link>
                     <div className="w-[50%] hidden h-full sm:flex items-center">
                         <CustomButton text="Sign Up" color="transparent" />
                     </div>
                 </div>
             </section>
-        </main>
+            <section className="dred flex flex-col xl:flex-row relative gap-5 z-0 max-w-[1440px] mx-auto">
+                <div className="flex-1 dblue pt-20 xl:pt-36 pl-3 h:px-6 sm:px-14">
+                    <h1 className="font-extrabold s:text-3xl text-white font-audio drop-shadow-lg md:text-6xl lg:text-7xl text-2xl h:text-4xl sm:text-5xl">
+                        TRANSCENDENCE
+                    </h1>
+                    <h2 className="text-2xl  h:text-4xl s:text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-roboto font-extrabold text-palette-orange drop-shadow-md mt-1 sm:mt-4 ">
+                        PONG GAME
+                    </h2>
+                    <h3 className="text-2xl h:text-3xl sm:text-4xl  xl:text-5xl font-teko drop-shadow-sm font-bold text-palette-white mt-8 xl:mt-24 s:mt-14">
+                        42 Project
+                    </h3>
+                    <p className="max-w-[600px] text-palette-grey font-light ">
+                        {landing_page_description}
+                    </p>
+                    <div className="min-h-[76px] hidden xl:flex items-center w-[64%] h-1 dred mt-6 xl:mt-10">
+                        <CustomButton text="GET STARTED" color="orange" />
+                    </div>
+                </div>
+
+                <div className="flex xl:hidden justify-center items-center dgreen w-full xl:h-screen">
+                    <div className="relative xl:w-full w-[80%] md:h-[590px] sm:h-[500px] h:h-[400px] h-[360px] dred flex justify-center z-0">
+                        <Image
+                            alt="machine"
+                            src="/sidelogo.png"
+                            fill
+                            className="object-contain dblue"
+                        />
+                    </div>
+                </div>
+
+                <div className="min-h-[40px] h:min-h-[76px] xl:hidden flex items-center justify-center w-[100%] h-1 dred mt-20 mb-20">
+                    <div className="sm:w-full w-[60%] h-full dblue flex justify-center max-w-[300px] items-center">
+                        <CustomButton text="GET STARTED" color="orange" />
+                    </div>
+                </div>
+            </section>
+            {/* //absolute */}
+            <div className="xl:flex aspect-square h-[55%] 2xl:h-[60%]  absolute -translate-y-[40%] top-[60%] right-[-115px] dred hidden justify-center z-0">
+                <Image
+                    alt="machine"
+                    src="/sidelogo.png"
+                    height={1000}
+                    width={1000}
+                    className="object-contain"
+                />
+            </div>
+        </div>
     );
 }
