4aeeb0eb74a6b5bd920b5d6efb8c3522c92ac9ff
Oussama  Zahid <ozahid-@e3r8p13.1337.ma>
2023-11-10 04:56:06 +0100

    Chore: add the header part of 2fa modal

--- components/auth/TwoFactor.tsx
+++ components/auth/TwoFactor.tsx
@@ -19,10 +19,16 @@ const TwoFactor = ({isOpen, closemodal}: TwoFactorProps) => {
               closemodal(false);
           }}
           withCorner={false}
-          customClass="absolute sm:h-[50%] sm:max-h-[520px] lg:max-h-[580px] h:max-h-[440px] max-h-[400px] max-w-[540px] h-[40%] md:w-[50%] w-[90%] s:w-[70%] min-h-[500px]"
+          customClass="absolute sm:h-[50%] max-h-[620px] max-w-[540px] h-[40%] md:w-[50%] w-[90%] s:w-[70%] h:min-h-[560px] min-h-[500px]"
       >
           <div className="flex items-center flex-col h-full overflow-auto">
-            
+              <div className="w-full flex justify-evenly items-end ">
+                  <hr className="w-[10%] border border-palette-grey rounded-sm" />
+                  <h2 className="text-gray-400 mt-4 font-body text-sm sm:text-xl lg:text-2xl font-bold ">
+                      Two Factor Autentication
+                  </h2>
+                  <hr className="w-[10%] border border-palette-grey rounded-sm" />
+              </div>
           </div>
       </MyDialog>
   );
