f63fcb40df05e85260a7edd4bc07bac0144d59c2
KharrazAdam <adam.kharraz13@gmail.com>
2023-11-11 08:09:23 +0100

    verfying log

--- you-pong-backend/src/guards/isLoggedIn.guard.ts
+++ you-pong-backend/src/guards/isLoggedIn.guard.ts
@@ -1,19 +1,19 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
 import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class isLoggedGuard implements CanActivate {
-  canActivate(context: ExecutionContext): boolean {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
 
     // Retrieve the value of the 'your-header-name' header
-    const token = request.headers['Authorization'];
+    const token = await request.headers['Authorization'];
     
     console.log(token);
     try {
       jwt.verify(token, process.env.JWT_SECRET)
     } catch(error) {
-      throw Error('awdii')
+      throw new ForbiddenException('You\'re already registred!')
     }
     return false;
   }
