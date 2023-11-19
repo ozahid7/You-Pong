995508e4e57d71ebf1e9bfcfa5790e69113f5c76
Oussama  Zahid <ozahid-@e3r8p13.1337.ma>
2023-11-20 00:16:13 +0100

    Fix: some styling in dashboard'

--- FrontEnd/app/hero/AchievementCard.tsx
+++ FrontEnd/app/hero/AchievementCard.tsx
@@ -26,7 +26,7 @@ const Achievements: Achievement[] = [
 const AchievementCard = () => {
     
   return (
-      <div className="flex min-w-[240px] xl:min-w-[280px] xl:max-w-[360px] md:w-[80%] w-[90%] h-[90%] ">
+      <div className="flex min-w-[240px] xl:min-w-[280px] xl:max-w-[360px] md:w-[80%] w-[90%] h-[80%] md:h-[90%] ">
           <MyCard type="nocorner" otherclass="max-h-full flex">
               <div className="w-full relative h-full flex flex-col justify-end items-end">
                     
@@ -43,7 +43,7 @@ const AchievementCard = () => {
                       opacity={1}
                       place={"top-start"}
                   />
-                  <div className=" flex w-full overflow-y-auto  md:h-[90%] h:h-[84%] h-[88%] space-y-20 flex-col items-center mb-2">
+                  <div className=" flex w-full overflow-y-auto  md:h-[90%] h:h-[84%] h-[86%] space-y-20 flex-col items-center mb-2">
                       {Achievements.map((e: Achievement, index) => (
                           <Acheivement
                               classname=""
