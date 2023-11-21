24138c8758cf72863ea9dff7a7454abd9aff7cc6
Abderrachid Yassir <ayassir@e1r9p7.1337.ma>
2023-11-21 21:30:45 +0100

    Add: Modal of join, working on it..

--- app/dashboard/chat/components/JoinModal.tsx
+++ app/dashboard/chat/components/JoinModal.tsx
@@ -17,12 +17,7 @@ import {
 } from "@nextui-org/react";
 import Image from "next/image";
 import groups from "../../../../public/groups.svg";
-import {
-  MyInput,
-  Background,
-  Submit,
-  MyContainer,
-} from "../../../../components";
+import { MyInput, Background, Submit } from "../../../../components";
 import { Channel } from "@/types";
 import { setData, setFile } from "@/app/dashboard/chat/data/api";
 import { LuLogIn, LuLock, LuUnlock } from "react-icons/lu";
@@ -67,67 +62,86 @@ export default function JoinModal() {
       >
         <ModalContent className="">
           {(close) => (
-              <Background>
-                <ModalHeader
-                  className="flex justify-center text-[#424242] text-shadow-xl font-['Chakra_Petch'] text-[40px] font-[700] leading-normal not-italic"
-                  style={{
-                    textShadow: "0px 2px 2px rgba(0, 0, 0, 0.25)",
-                  }}
-                >
-                  Join a group
-                </ModalHeader>
-                <ModalBody className="w-[95%]">
-                  <div className="flex justify-evenly items-center flex-col gap-3">
-                    <div className=" flex items-center flex-col justify-evenly w-full h-full gap-2">
-                      <div className="overflow-x-auto">
-                        <table className="table border-white">
-                          {/*<!-- head -->*/}
-                          <thead>
-                            <tr>
-                              <th></th>
-                              <th>Name</th>
-                              <th>Job</th>
-                              <th>Favorite Color</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            {/*<!-- row 1 -->*/}
-                            <tr>
-                              <th>1</th>
-                              <td>Cy Ganderton</td>
-                              <td>Quality Control Specialist</td>
-                              <td>Blue</td>
-                            </tr>
-                            {/*<!-- row 2 -->*/}
-                            <tr>
-                              <th>2</th>
-                              <td>Hart Hagerty</td>
-                              <td>Desktop Support Technician</td>
-                              <td>Purple</td>
-                            </tr>
-                            {/*<!-- row 3 -->*/}
-                            <tr>
-                              <th>3</th>
-                              <td>Brice Swyre</td>
-                              <td>Tax Accountant</td>
-                              <td>Red</td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>
+            <Background>
+              <ModalHeader
+                className="flex justify-center text-[#424242] text-shadow-xl font-['Chakra_Petch'] text-[40px] font-[700] leading-normal not-italic"
+                style={{
+                  textShadow: "0px 2px 2px rgba(0, 0, 0, 0.25)",
+                }}
+              >
+                Join a group
+              </ModalHeader>
+              <ModalBody className="w-[95%]">
+                <div className="flex justify-evenly items-center flex-col gap-3">
+                  <div className=" flex items-center flex-col justify-evenly w-full h-full gap-2">
+                    <Table aria-label="Example empty table">
+                      <TableHeader>
+                        <TableColumn className="font-['Chakra_Petch'] font-[600] text-shadow-md">
+                          NAME
+                        </TableColumn>
+                        <TableColumn className="font-['Chakra_Petch'] font-[600] text-shadow-md">
+                          TYPE
+                        </TableColumn>
+                        <TableColumn className="font-['Chakra_Petch'] font-[600] text-shadow-md">
+                          ACTION
+                        </TableColumn>
+                      </TableHeader>
+                      <TableBody emptyContent={"No channels to display."}>
+                        <TableRow>
+                          <TableCell className="text-[#424242] text-shadow-xl font-['Chakra_Petch'] text-[18px] font-[700] leading-normal not-italic">
+                            Channel1
+                          </TableCell>
+                          <TableCell className="font-archivo text-[18px] text-palette-orange">
+                            <div className="flex flex-row gap-2 items-center">
+                              PROTECTED
+                              <LuLock />
+                            </div>
+                          </TableCell>
+                          <TableCell className="flex flex-row">
+                            <Button
+                              size="md"
+                              className="flex text-[20px] btn xs:btn-xs sm:btn-sm md:btn-md font-body font-[600] text-[#EFF5F5] green_button rounded-md border-none hover:border-none"
+                            >
+                              <LuLogIn />
+                              Join
+                            </Button>
+                          </TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell className="text-[#424242] text-shadow-xl font-['Chakra_Petch'] text-[18px] font-[700] leading-normal not-italic">
+                            Channel1
+                          </TableCell>
+                          <TableCell className="font-archivo text-[18px] text-palette-orange">
+                            <div className="flex flex-row gap-2 items-center">
+                              PUBLIC
+                              <LuUnlock />
+                            </div>
+                          </TableCell>
+                          <TableCell className="flex flex-row h-full">
+                            <Button
+                              size="md"
+                              className="flex text-[20px] btn xs:btn-xs sm:btn-sm md:btn-md  font-body font-[600] text-[#EFF5F5] rounded-md green_button border-none hover:border-none"
+                            >
+                              <LuLogIn />
+                              Join
+                            </Button>
+                          </TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
                   </div>
-                </ModalBody>
-                <ModalFooter>
-                  <div className="flex w-[300px] h-[70px]">
-                    <Submit
-                      color="orange"
-                      text="JOIN"
-                      // handleclick={}
-                    ></Submit>
-                  </div>
-                </ModalFooter>
-              </Background>
+                </div>
+              </ModalBody>
+              <ModalFooter>
+                <div className="flex w-[300px] h-[70px]">
+                  <Submit
+                    color="orange"
+                    text="JOIN"
+                    // handleclick={}
+                  ></Submit>
+                </div>
+              </ModalFooter>
+            </Background>
           )}
         </ModalContent>
       </Modal>
