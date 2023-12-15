71165fa2b8726ef03e994d55adfb3da95f776290
Abdelhamid Jafy <ajafy@e1r9p9.1337.ma>
2023-12-15 05:23:00 +0100

    friends created

--- backend/src/user/services/find.service.ts
+++ backend/src/user/services/find.service.ts
@@ -1,48 +1,50 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class FindUserService {
-    constructor(private prisma:PrismaService){}
-    async finduserById(_id: string){
-        const user = await this.prisma.user.findUnique({
-            where:{
-                id_user: _id
-            },
-            include: {
-                freindship_freind: true,
-                freindship_user: true,
-                channels: true,
-            },
-        });
-        return (user);
-    };
+  constructor(private prisma: PrismaService) {}
+  async finduserById(_id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id_user: _id,
+      },
+      include: {
+        friendship_friend: true,
+        friendship_user: true,
+        channels: true,
+        owned: true,
+        matchs: true,
+      },
+    });
+    return user;
+  }
 
-    async finduserByUserName(_username: string){
-        const user = await this.prisma.user.findUnique({
-            where:{
-                username: _username
-            },
-            include:{
-                freindship_freind: true,
-                freindship_user:true,
-                channels: true,
-            },
-        });
-        return (user);
-    };
-    
-    async finduserByEmail(_email: string){
-        const user = await this.prisma.user.findUnique({
-            where:{
-                email: _email
-            },
-            include:{
-                freindship_freind: true,
-                freindship_user:true,
-                channels: true,
-            }
-        });
-        return (user);
-    };
+  async finduserByUserName(_username: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        username: _username,
+      },
+      include: {
+        friendship_friend: true,
+        friendship_user: true,
+        channels: true,
+      },
+    });
+    return user;
+  }
+
+  async finduserByEmail(_email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: _email,
+      },
+      include: {
+        friendship_friend: true,
+        friendship_user: true,
+        channels: true,
+      },
+    });
+    return user;
+  }
 }
