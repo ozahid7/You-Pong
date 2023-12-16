6a7073289cc12755fc1ccdf178d9ba90f8ef9b5a
junik1337 <Abderrachidyassir@gmail.com>
2023-12-16 15:09:10 +0100

    Fix: After merge

--- FrontEnd/app/(main)/chat/components/SearchChat.tsx
+++ FrontEnd/app/(main)/chat/components/SearchChat.tsx
@@ -4,12 +4,6 @@ import { Combobox, Transition } from "@headlessui/react";
 import { LuSearch } from "react-icons/lu";
 import { Channel } from "@/types";
 import { GroupsChat } from ".";
-"use client";
-import { Fragment, useEffect, useState } from "react";
-import { Combobox, Transition } from "@headlessui/react";
-import { LuSearch } from "react-icons/lu";
-import { Channel } from "@/types";
-import { GroupsChat } from ".";
 
 interface friend {
   userName: string;
