8a7700919ab5263155f64ca94bf95b7f42927d0e
junik1337 <abderrachidyassir@gmail.com>
2024-01-01 03:32:09 +0100

    Fix: Fix Dropdown direct

--- FrontEnd/app/(main)/chat/[user]/components/MiniChatDirect.tsx
+++ FrontEnd/app/(main)/chat/[user]/components/MiniChatDirect.tsx
@@ -29,7 +29,7 @@ const MiniChatDirect = ({ channels, main }: HomeProps) => {
     : null;
 
   return (
-    <div className="flex w-full h-full 3xl_:w-[19.5rem] 2xl_:w-[18rem] xl_:w-[14.5rem] lg_:w-[13rem] md_:w-[8rem] rounded-sm shadow-md shadow-[rgba(0,0,0,0.25)] items-center justify-evenly flex-row bg-[#EFF5F5]">
+    <div className="flex w-full h-[60px] 3xl_:w-[19.5rem] 2xl_:w-[18rem] xl_:w-[14.5rem] lg_:w-[13rem] md_:w-[8rem] rounded-sm shadow-md items-center justify-evenly flex-row bg-[#EFF5F5]">
       <Avatar
         isBordered
         radius="sm"
@@ -37,8 +37,8 @@ const MiniChatDirect = ({ channels, main }: HomeProps) => {
         className="flex w-[44px] h-[44px] "
         src={user?.avatar}
       />
-      <div className="w-[70%] h-[80%] overflow-hidden xxs:hidden sm:block">
-        <p className="text-[#424242] font-archivo font-[800] text-[19px]">
+      <div className="flex w-[70%] h-[80%] overflow-hidden xxs:hidden sm:block text-[#424242] font-archivo font-[800] text-[19px] flex-col content-center debug">
+        <p className="whitespace-nowrap overflow-hidden text-ellipsis">
           {user?.username}
         </p>
       </div>
