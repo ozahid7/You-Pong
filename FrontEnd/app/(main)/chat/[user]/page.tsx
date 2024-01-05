25b5bf2e57a63ae19b5bb801f5afc062a9595a25
junik1337 <abderrachidyassir@gmail.com>
2024-01-05 10:47:16 +0100

    New: changing the socket...

--- FrontEnd/app/(main)/chat/[user]/page.tsx
+++ FrontEnd/app/(main)/chat/[user]/page.tsx
@@ -30,7 +30,7 @@ import { Channel, QueryProp, User, User_Hero, whichChannel } from "@/types";
 import { io } from "socket.io-client";
 import { useQuery } from "react-query";
 import Loader from "@/components/tools/Loader";
-import { redirect } from "next/navigation";
+import { useGlobalSocket } from "@/providers/UserContextProvider";
 
 var one: boolean = false;
 var connection: any = null;
@@ -47,6 +47,8 @@ const Chats = ({ params }) => {
   const [value, setValue] = useState<number>(0);
   const [valueDirect, setValueDirect] = useState<number>(0);
   const [valueGroups, setValueGroups] = useState<number>(0);
+  const { globalSocket } = useGlobalSocket();
+  let setGlobal = globalSocket.io;
 
   const { data: MainUser, isLoading: MainLoading } = useQuery<User_Hero, Error>(
     ["MainUser"],
@@ -171,183 +173,169 @@ const Chats = ({ params }) => {
     });
   };
 
-  //// Socket
+  // Socket
   if (MainUser?.uid && !one) {
-    connection = io(`http://localhost:4000/chat?id_user=${MainUser.uid}`, {
-      transports: ["websocket"],
-      transportOptions: {
-        polling: {
-          extraHeaders: {
-            "Sec-WebSocket-Version": "13",
-            "Sec-WebSocket-Key": "0Me1PSdr2zimQ28+k6ug8w==",
-            "Sec-WebSocket-Extensions":
-              "permessage-deflate; client_max_window_bits",
-          },
-        },
-      },
-      autoConnect: true,
-    });
+    connection = setGlobal.socket("/chat");
     one = true;
+    console.log("mysocket", connection);
   }
 
   return (
     <div className="h-full min-h-[600px] w-full make_center">
       <div className="flex justify-center w-[90%] max-w-[1200px] min-w-[260px] min-h-[600px] h-[90%]">
         <MyContainer>
-            <Background>
-              <div className="w-[95%] h-full">
-                <div className="flex flex-row w-full h-full items-center ">
-                  <div className="flex h-[90%] w-[35%] flex-col justify-evenly gap-5 border-r-white border-r-[2px] border-solid pr-5">
-                    <ChatHeading text="Chats" />
-                    <SearchChat
-                      object={channelsGroups}
-                      direct={channelsDirect}
-                      main={MainUser}
-                      onResultIdChange={handleResultId}
-                    />
-                    <div className="flex h-full w-full flex-row justify-center items-center">
-                      <div className="flex h-full w-full flex-col gap-5 justify-center items-center ">
-                        <div className="flex flex-row w-fit h-fit ">
-                          <MyTabs
-                            value={value}
-                            onChange={(value) => {
-                              setValue(value);
-                            }}
-                            labels={[
-                              <div className="flex w-fit h-fit text-[#686868] font-[600] 2xl_:text-[140%] xl_:text-[120%] lg_:text-[110%] sm_:text-[250%] font-archivo self-center gap-1 items-center">
-                                <LuUser className="text-[white]" />
-                                <div className="xxs:hidden lg_:block">
-                                  DIRECT
-                                </div>
-                              </div>,
-                              <div className="flex w-fit h-fit text-[#686868] font-[600] 2xl_:text-[140%] xl_:text-[120%] lg_:text-[110%] sm_:text-[250%] font-archivo self-center gap-1 items-center">
-                                <LuUsers className="text-[white]" />
-                                <div className="xxs:hidden lg_:block">
-                                  CHANNELS
-                                </div>
-                              </div>,
-                            ]}
-                            indicator={{
-                              className:
-                                "bg-white self-center lg_:w-[98%] sm_:w-[92%]",
-                            }}
-                          ></MyTabs>
-                        </div>
-                        <div className="flex h-full w-full">
-                          <SwipeableTabs
-                            value={value}
-                            className="flex h-full w-full justify-center overflow-x-hidden"
-                          >
-                            <div className="flex w-full h-full justify-start items-center flex-col">
-                              <MyTabs
-                                value={valueDirect}
-                                className="flex flex-col self-center w-fit h-fit"
-                                onChange={(valueDirect) => {
-                                  setValueDirect(valueDirect);
-                                }}
-                                labels={
-                                  channelsDirect
-                                    ? channelsDirect.map((obj: Channel, i) => (
-                                        <MiniChatDirect
+          <Background>
+            <div className="w-[95%] h-full">
+              <div className="flex flex-row w-full h-full items-center ">
+                <div className="flex h-[90%] w-[35%] flex-col justify-evenly gap-5 border-r-white border-r-[2px] border-solid pr-5">
+                  <ChatHeading text="Chats" />
+                  <SearchChat
+                    object={channelsGroups}
+                    direct={channelsDirect}
+                    main={MainUser}
+                    onResultIdChange={handleResultId}
+                  />
+                  <div className="flex h-full w-full flex-row justify-center items-center">
+                    <div className="flex h-full w-full flex-col gap-5 justify-center items-center ">
+                      <div className="flex flex-row w-fit h-fit ">
+                        <MyTabs
+                          value={value}
+                          onChange={(value) => {
+                            setValue(value);
+                          }}
+                          labels={[
+                            <div className="flex w-fit h-fit text-[#686868] font-[600] 2xl_:text-[140%] xl_:text-[120%] lg_:text-[110%] sm_:text-[250%] font-archivo self-center gap-1 items-center">
+                              <LuUser className="text-[white]" />
+                              <div className="xxs:hidden lg_:block">DIRECT</div>
+                            </div>,
+                            <div className="flex w-fit h-fit text-[#686868] font-[600] 2xl_:text-[140%] xl_:text-[120%] lg_:text-[110%] sm_:text-[250%] font-archivo self-center gap-1 items-center">
+                              <LuUsers className="text-[white]" />
+                              <div className="xxs:hidden lg_:block">
+                                CHANNELS
+                              </div>
+                            </div>,
+                          ]}
+                          indicator={{
+                            className:
+                              "bg-white self-center lg_:w-[98%] sm_:w-[92%]",
+                          }}
+                        ></MyTabs>
+                      </div>
+                      <div className="flex h-full w-full">
+                        <SwipeableTabs
+                          value={value}
+                          className="flex h-full w-full justify-center overflow-x-hidden"
+                        >
+                          <div className="flex w-full h-full justify-start items-center flex-col">
+                            <MyTabs
+                              value={valueDirect}
+                              className="flex flex-col self-center w-fit h-fit"
+                              onChange={(valueDirect) => {
+                                setValueDirect(valueDirect);
+                              }}
+                              labels={
+                                channelsDirect
+                                  ? channelsDirect.map((obj: Channel, i) => (
+                                      <MiniChatDirect
+                                        channels={obj}
+                                        main={MainUser}
+                                        key={i}
+                                      />
+                                    ))
+                                  : []
+                              }
+                              indicator={{
+                                className:
+                                  "bg-palette-green self-center ml-[2.5%] lg_:w-[20%] lg_:h-[8%] ",
+                              }}
+                              key="direct"
+                            />
+                          </div>
+                          <div className="flex w-full h-full justify-start items-center flex-col gap-2 ">
+                            <MyTabs
+                              value={valueGroups}
+                              className="flex flex-col flex-grow w-fit h-fit"
+                              onChange={(valueGroups) => {
+                                setValueGroups(valueGroups);
+                              }}
+                              labels={
+                                channelsGroups
+                                  ? channelsGroups.map((obj: any, i) => {
+                                      return (
+                                        <MiniChat
                                           channels={obj}
-                                          main={MainUser}
                                           key={i}
                                         />
-                                      ))
-                                    : []
-                                }
-                                indicator={{
-                                  className:
-                                    "bg-palette-green self-center ml-[2.5%] lg_:w-[20%] lg_:h-[8%] ",
-                                }}
-                                key="direct"
+                                      );
+                                    })
+                                  : []
+                              }
+                              indicator={{
+                                className:
+                                  "bg-palette-green self-center 2xl:w-[52px] 2xl:ml-3 xl:w-[50px] xl:ml-2 lg:w-[48px] lg:ml-2 md:w-[44px] md:ml-1",
+                              }}
+                              key="groups"
+                            />
+                            <NextUIProvider className="flex w-[90%] lg:flex-row xs:flex-col justify-evenly items-center gap-2">
+                              <CreateModal refetch={refetch} />
+                              <JoinModal
+                                refetch={refetch}
+                                channels={JoinChannels}
                               />
-                            </div>
-                            <div className="flex w-full h-full justify-start items-center flex-col gap-2 ">
-                              <MyTabs
-                                value={valueGroups}
-                                className="flex flex-col flex-grow w-fit h-fit"
-                                onChange={(valueGroups) => {
-                                  setValueGroups(valueGroups);
-                                }}
-                                labels={
-                                  channelsGroups
-                                    ? channelsGroups.map((obj: any, i) => {
-                                        return (
-                                          <MiniChat
-                                            channels={obj}
-                                            key={i}
-                                          />
-                                        );
-                                      })
-                                    : []
-                                }
-                                indicator={{
-                                  className:
-                                    "bg-palette-green self-center 2xl:w-[52px] 2xl:ml-3 xl:w-[50px] xl:ml-2 lg:w-[48px] lg:ml-2 md:w-[44px] md:ml-1",
-                                }}
-                                key="groups"
-                              />
-                              <NextUIProvider className="flex w-[90%] lg:flex-row xs:flex-col justify-evenly items-center gap-2">
-                                <CreateModal refetch={refetch} />
-                                <JoinModal
-                                  refetch={refetch}
-                                  channels={JoinChannels}
-                                />
-                              </NextUIProvider>
-                            </div>
-                          </SwipeableTabs>
-                        </div>
+                            </NextUIProvider>
+                          </div>
+                        </SwipeableTabs>
                       </div>
                     </div>
                   </div>
-                  {value ? (
-                    <SwipeableTabs
-                      value={valueGroups}
-                      className="h-full w-full  flex-1  overflow-x-hidden my_scroll_green"
-                      key="groups"
-                    >
-                      {channelsGroups &&
-                        channelsGroups.map((obj, i) => {
-                          return (
-                            <GroupsChat
-                              data={channelsGroups}
-                              channels={obj}
-                              socket={connection}
-                              MainUser={MainUser}
-                              indexChannels={indexChannels}
-                              index={valueGroups}
-                              channelsRefetch={refetch}
-                              joinRefetch={joinRefetch}
-                              key={i}
-                            ></GroupsChat>
-                          );
-                        })}
-                    </SwipeableTabs>
-                  ) : (
-                    <SwipeableTabs
-                      value={valueDirect}
-                      className="h-full w-full  flex-1  overflow-x-hidden"
-                      key="direct"
-                    >
-                      {channelsDirect &&
-                        channelsDirect.map((obj, i) => (
-                          <Chat
-                            data={channelsDirect}
+                </div>
+                {value ? (
+                  <SwipeableTabs
+                    value={valueGroups}
+                    className="h-full w-full  flex-1  overflow-x-hidden my_scroll_green"
+                    key="groups"
+                  >
+                    {channelsGroups &&
+                      channelsGroups.map((obj, i) => {
+                        return (
+                          <GroupsChat
+                            data={channelsGroups}
                             channels={obj}
                             socket={connection}
-                            main={MainUser}
-                            indexChannels={indexChannelsDirect}
-                            index={valueDirect}
-                            directRefetch={directRefetch}
+                            MainUser={MainUser}
+                            indexChannels={indexChannels}
+                            index={valueGroups}
+                            channelsRefetch={refetch}
+                            joinRefetch={joinRefetch}
                             key={i}
-                          ></Chat>
-                        ))}
-                    </SwipeableTabs>
-                  )}
-                </div>
+                          ></GroupsChat>
+                        );
+                      })}
+                  </SwipeableTabs>
+                ) : (
+                  <SwipeableTabs
+                    value={valueDirect}
+                    className="h-full w-full  flex-1  overflow-x-hidden"
+                    key="direct"
+                  >
+                    {channelsDirect &&
+                      channelsDirect.map((obj, i) => (
+                        <Chat
+                          data={channelsDirect}
+                          channels={obj}
+                          socket={connection}
+                          main={MainUser}
+                          indexChannels={indexChannelsDirect}
+                          index={valueDirect}
+                          directRefetch={directRefetch}
+                          key={i}
+                        ></Chat>
+                      ))}
+                  </SwipeableTabs>
+                )}
               </div>
-            </Background>
+            </div>
+          </Background>
         </MyContainer>
       </div>
     </div>
