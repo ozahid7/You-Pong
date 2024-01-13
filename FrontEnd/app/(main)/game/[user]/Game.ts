fbdd4e8884bed30d20346c788c57158c50a24798
junik1337 <abderrachidyassir@gmail.com>
2024-01-13 01:40:11 +0100

    Trying to fix responsive

--- FrontEnd/app/(main)/game/[user]/Game.ts
+++ FrontEnd/app/(main)/game/[user]/Game.ts
@@ -1,239 +1,306 @@
 import * as Matter from "matter-js";
 
 import {
-	getBall,
-	getBottomPaddle,
-	getBottomWall,
-	getLeftWall,
-	getRightWall,
-	getTopPaddle,
-	getTopWall,
+  getBall,
+  getBottomPaddle,
+  getBottomWall,
+  getLeftWall,
+  getRightWall,
+  getTopPaddle,
+  getTopWall,
 } from "./gameUtils";
 import { Socket } from "socket.io-client";
 import { ball, opponent, player } from "./GameProvider";
 import { inviteReturn } from "@/types/game";
 
 export interface Scores {
-	player: number;
-	opponent: number;
+  player: number;
+  opponent: number;
 }
 
 export const {
-	Engine,
-	Render,
-	World,
-	Bodies,
-	Composite,
-	Body,
-	Events,
-	Mouse,
-	Runner,
-	MouseConstraint,
+  Engine,
+  Render,
+  World,
+  Bodies,
+  Composite,
+  Body,
+  Events,
+  Mouse,
+  Runner,
+  MouseConstraint,
 } = Matter;
 
 export class Game {
-	height: number;
-	width: number;
-	render: Matter.Render;
-	engine: Matter.Engine;
-	topPaddle: Matter.Body;
-	bottomPaddle: Matter.Body;
-	ball: Matter.Body;
-	walls: Matter.Body[] = [];
-	mouse: Matter.Mouse;
-	mouseConstraint: Matter.mouseConstraint;
-	socket: Socket;
-	paddleSize: number;
-	gameData: inviteReturn;
-	tmpX: number = 0;
-	scores: Scores;
-	interval: any;
+  height: number;
+  width: number;
+  render: Matter.Render;
+  engine: Matter.Engine;
+  topPaddle: Matter.Body;
+  bottomPaddle: Matter.Body;
+  ball: Matter.Body;
+  walls: Matter.Body[] = [];
+  mouse: Matter.Mouse;
+  mouseConstraint: Matter.mouseConstraint;
+  socket: Socket;
+  paddleSize: number;
+  gameData: inviteReturn;
+  tmpX: number = 0;
+  scores: Scores;
+  interval: any;
+  element: HTMLElement;
+  scale: number = 1;
 
-	constructor(
-		container: HTMLDivElement,
-		map: string,
-		socket: Socket,
-		mode: string,
-		gameData: inviteReturn
-	) {
-		this.height = container.clientHeight;
-		this.width = container.clientWidth;
+  constructor(
+    container: HTMLDivElement,
+    map: string,
+    socket: Socket,
+    mode: string,
+    gameData: inviteReturn
+  ) {
+    this.element = container;
 
-		this.socket = socket;
-		this.gameData = gameData;
+    this.socket = socket;
+    this.gameData = gameData;
 
-		let strokeColor: string;
-		let fillColor: string;
-		let background: string;
+    let strokeColor: string;
+    let fillColor: string;
+    let background: string;
 
-		this.paddleSize = mode === "easy" ? 160 : 100;
-		if (map === "orange") {
-			fillColor = "#EB6440";
-			strokeColor = "#EB6440";
-			background = "white";
-		} else if (map === "green") {
-			fillColor = "#497174";
-			strokeColor = "#497174";
-			background = "white";
-		} else {
-			fillColor = "#ffffff";
-			strokeColor = "#ffffff";
-			background = "black";
-		}
+    this.paddleSize = mode === "easy" ? 160 : 100;
+    if (map === "orange") {
+      fillColor = "#EB6440";
+      strokeColor = "#EB6440";
+      background = "white";
+    } else if (map === "green") {
+      fillColor = "#497174";
+      strokeColor = "#497174";
+      background = "white";
+    } else {
+      fillColor = "#ffffff";
+      strokeColor = "#ffffff";
+      background = "black";
+    }
 
-		const wallOptions = {
-			isStatic: true,
-			render: {
-				fillStyle: fillColor,
-				strokeStyle: strokeColor,
-			},
-		};
+    const wallOptions = {
+      isStatic: true,
+      render: {
+        fillStyle: fillColor,
+        strokeStyle: strokeColor,
+      },
+    };
+	
+    [this.width, this.height] = this.FixSizeRatio();
+    this.ScaleUpdate();
 
-		this.engine = Engine.create();
+    this.engine = Engine.create({ gravity: { x: 0, y: 0 } });
 
-		// Create a render
-		this.render = Render.create({
-			element: container,
-			engine: this.engine,
-			options: {
-				width: this.width,
-				height: this.height,
-				wireframes: false,
-				background: background,
-				chamfer: { radius: 60 },
-			},
-		});
+    // Create a render
+    this.render = Render.create({
+      element: this.element,
+      engine: this.engine,
+      options: {
+        width: this.width,
+        height: this.height,
+        wireframes: false,
+        background: background,
+        chamfer: { radius: 60 },
+      },
+    });
 
-		//init players
-		this.bottomPaddle = getBottomPaddle(
-			this.width,
-			this.height,
-			{
-				isStatic: true,
-				render: {
-					fillStyle: fillColor,
-					strokeStyle: strokeColor,
-				},
-				chamfer: { radius: [7, 7, 0, 0] },
-			},
-			this.paddleSize
-		);
+    //init players
+    this.bottomPaddle = getBottomPaddle(
+      this.width,
+      this.height,
+      {
+        isStatic: true,
+        render: {
+          fillStyle: fillColor,
+          strokeStyle: strokeColor,
+        },
+        chamfer: { radius: [7, 7, 0, 0] },
+      },
+      this.remap(this.paddleSize, 0, 600, 0, this.width),
+      this.remap(15, 0, 800, 0, this.height),
+      this.remap(50, 0, 800, 0, this.height)
+    );
 
-		this.tmpX = this.bottomPaddle.position.x;
-		this.topPaddle = getTopPaddle(
-			this.width,
-			{
-				isStatic: true,
-				render: {
-					fillStyle: fillColor,
-					strokeStyle: strokeColor,
-				},
-				chamfer: { radius: [0, 0, 7, 7] },
-			},
-			this.paddleSize
-		);
+    this.topPaddle = getTopPaddle(
+      this.width,
+      {
+        isStatic: true,
+        render: {
+          fillStyle: fillColor,
+          strokeStyle: strokeColor,
+        },
+        chamfer: { radius: [0, 0, 7, 7] },
+      },
+      this.remap(this.paddleSize, 0, 600, 0, this.width),
+      this.remap(15, 0, 800, 0, this.height),
+      this.remap(50, 0, 800, 0, this.height)
+    );
 
-		//init walls
-		this.walls.push(getTopWall(this.width, Bodies, wallOptions));
-		this.walls.push(
-			getBottomWall(this.height, this.width, Bodies, wallOptions)
-		);
-		this.walls.push(getLeftWall(this.height, Bodies, wallOptions));
-		this.walls.push(
-			getRightWall(this.height, this.width, Bodies, wallOptions)
-		);
+    //init walls
+    this.walls.push(
+      getTopWall(
+        this.width,
+        Bodies,
+        wallOptions,
+        this.remap(10, 0, 800, 0, this.height)
+      )
+    );
+    this.walls.push(
+      getBottomWall(
+        this.height,
+        this.width,
+        Bodies,
+        wallOptions,
+        this.remap(10, 0, 800, 0, this.height)
+      )
+    );
+    this.walls.push(
+      getLeftWall(
+        this.height,
+        Bodies,
+        wallOptions,
+        this.remap(10, 0, 600, 0, this.width)
+      )
+    );
+    this.walls.push(
+      getRightWall(
+        this.height,
+        this.width,
+        Bodies,
+        wallOptions,
+        this.remap(10, 0, 600, 0, this.width)
+      )
+    );
 
-		//init ball
-		this.ball = getBall(this.width, this.height, wallOptions);
+    //init ball
+    this.ball = getBall(this.width, this.height, this.scale, wallOptions);
 
-		this.mouse = Matter.Mouse.create(this.render.canvas);
-		this.mouseConstraint = Matter.MouseConstraint.create(this.engine, {
-			mouse: this.mouse,
-		});
+    this.mouse = Matter.Mouse.create(this.render.canvas);
+    this.mouseConstraint = Matter.MouseConstraint.create(this.engine, {
+      mouse: this.mouse,
+    });
 
-		this.mouse = Matter.Mouse.create(this.render.canvas);
-		this.mouseConstraint = Matter.MouseConstraint.create(this.engine, {
-			mouse: this.mouse,
-		});
+    this.mouse = Matter.Mouse.create(this.render.canvas);
+    this.mouseConstraint = Matter.MouseConstraint.create(this.engine, {
+      mouse: this.mouse,
+    });
 
-		// Add all the bodies to the world
-		World.add(this.engine.world, [
-			...this.walls,
-			this.topPaddle,
-			this.bottomPaddle,
-			this.ball,
-		]);
-		World.add(this.engine.world, this.mouseConstraint);
+    // Add all the bodies to the world
+    World.add(this.engine.world, [
+      ...this.walls,
+      this.topPaddle,
+      this.bottomPaddle,
+      this.ball,
+    ]);
+    World.add(this.engine.world, this.mouseConstraint);
 
-		// Run the engine
-		Runner.run(this.engine);
+    // Run the engine
+    Runner.run(this.engine);
 
-		// Run the renderer
-		Render.run(this.render);
+    // Run the renderer
+    Render.run(this.render);
 
-		// Set up mouse events
-		setTimeout(() => {
-			this.emitToUpdateFrame();
-		}, 3000);
-		this.setupMouseEvents();
-	}
+    // Set up mouse events
+    setTimeout(() => {
+      this.emitToUpdateFrame();
+    }, 3000);
+    this.setupMouseEvents();
+  }
 
-	emitToUpdateFrame() {
-		this.interval = setInterval(() => {
-			this.socket.emit("updateFrame", {
-				paddleX: this.tmpX,
-				id_match: this.gameData.id_match,
-			});
-		}, 1000 / 60);
-	}
+  remap(value: number, x1: number, y1: number, x2: number, y2: number): number {
+    return ((value - x1) * (y2 - x2)) / (y1 - x1 + x2);
+  }
 
-	updateBallPosition(data: ball) {
-		Matter.Body.setPosition(this.ball, {
-			x: data.x,
-			y: data.y,
-		});
-	}
+  FixSizeRatio(): number[] {
+    let width: number, height: number;
+    let aspect = 600 / 800;
 
-	updateOpponentPosition(data: opponent) {
-		Matter.Body.setPosition(this.topPaddle, {
-			x: data.x,
-			y: this.topPaddle.position.y,
-		});
-	}
+    if (this.element.clientWidth < this.element.clientHeight) {
+      width = this.element.clientWidth;
+      height = (width * 1) / aspect;
+      if (height > this.element.clientHeight) {
+        height = this.element.clientHeight;
+        width = height * aspect;
+      }
+    } else {
+      height = this.element.clientHeight;
+      width = height * aspect;
+      if (width > this.element.clientWidth) {
+        width = this.element.clientWidth;
+        height = (width * 1) / aspect;
+      }
+    }
+    return [width, height];
+  }
 
-	updatePlayerPosition(data: player) {
-		Matter.Body.setPosition(this.bottomPaddle, {
-			x: data.x,
-			y: this.bottomPaddle.position.y,
-		});
-	}
+  ScaleUpdate() {
+    const ScaleWidth: number = this.width / 600;
+    const ScaleHeight: number = this.height / 800;
 
-	setupMouseEvents() {
-		const max = this.width - this.paddleSize / 2;
-		const min = this.paddleSize / 2;
+    this.scale = ScaleWidth < ScaleHeight ? ScaleWidth : ScaleHeight;
+  }
 
-		Matter.Events.on(
-			this.mouseConstraint,
-			"mousemove",
-			(event: Matter.IMouseEvent) => {
-				if (
-					this.mouse.position.x < max &&
-					this.mouse.position.x > min
-				) {
-					this.tmpX = this.mouse.position.x;
-				}
-			}
-		);
-	}
+  emitToUpdateFrame() {
+    this.interval = setInterval(() => {
+      this.socket.emit("updateFrame", {
+        paddleX: this.tmpX,
+        id_match: this.gameData?.id_match,
+      });
+    }, 1000 / 60);
+  }
 
-	stopIntervall() {
-		clearInterval(this.interval);
-	}
+  updateBallPosition(data: ball) {
+    if (data)
+      Matter.Body.setPosition(this.ball, {
+        x: this.remap(data.x, 0, 600, 0, this.width), // **
+        y: this.remap(data.y, 0, 800, 0, this.height), // normalizing dial size, bash t9ad f responsive
+      });
+  }
 
-	destroy() {
-		World.clear(this.engine.world, true);
-		Render.stop(this.render);
-		Engine.clear(this.engine);
-		Events.off(this.engine, "mousemove");
-	}
+  updateOpponentPosition(data: opponent) {
+    if (data)
+      Matter.Body.setPosition(this.topPaddle, {
+        x: this.remap(data.x, 0, 600, 0, this.width), // **
+        y: this.topPaddle.position.y,
+      });
+  }
+
+  updatePlayerPosition(data: player) {
+    if (data)
+      Matter.Body.setPosition(this.bottomPaddle, {
+        x: this.remap(data.x, 0, 600, 0, this.width), // **
+        y: this.bottomPaddle.position.y,
+      });
+  }
+
+  setupMouseEvents() {
+    const max = this.width - this.paddleSize / 2;
+    const min = this.paddleSize / 2;
+
+    Matter.Events.on(
+      this.mouseConstraint,
+      "mousemove",
+      (event: Matter.IMouseEvent) => {
+        if (this.mouse.position.x < max && this.mouse.position.x > min) {
+          this.tmpX = this.mouse.position.x;
+        }
+      }
+    );
+  }
+
+  stopIntervall() {
+    clearInterval(this.interval);
+  }
+
+  destroy() {
+    this.render.canvas.remove();
+    World.clear(this.engine.world, true);
+    Render.stop(this.render);
+    Engine.clear(this.engine);
+    Events.off(this.engine, "mousemove");
+  }
 }
--- FrontEnd/app/(main)/game/[user]/gameUtils.ts
+++ FrontEnd/app/(main)/game/[user]/gameUtils.ts
@@ -1,111 +1,106 @@
 import Matter from "matter-js";
 export const myRender = (
-	container: any,
-	engine: any,
-	width: number,
-	height: number,
-	background: string
+  container: any,
+  engine: any,
+  width: number,
+  height: number,
+  background: string
 ) => {
-	return Matter.Render.create({
-		element: container,
-		engine: engine,
-		options: {
-			width: width,
-			height: height,
-			wireframes: false,
-			background: background,
-			chamfer: { radius: 60 },
-		},
-	});
+  return Matter.Render.create({
+    element: container,
+    engine: engine,
+    options: {
+      width: width,
+      height: height,
+      wireframes: false,
+      background: background,
+      chamfer: { radius: 60 },
+    },
+  });
 };
 
-const wallThickness = 10;
-
-export const getTopWall = (width: number, Bodies: any, wallOptions: object) => {
-	return Bodies.rectangle(
-		width / 2,
-		wallThickness / 2,
-		width + 2,
-		wallThickness,
-		wallOptions
-	);
+export const getTopWall = (
+  width: number,
+  Bodies: any,
+  wallOptions: object,
+  thicc: number
+) => {
+  return Bodies.rectangle(width / 2, thicc / 2, width + 2, thicc, wallOptions);
 };
 
 export const getBottomWall = (
-	height: number,
-	width: number,
-	Bodies: any,
-	wallOptions: object
+  height: number,
+  width: number,
+  Bodies: any,
+  wallOptions: object,
+  thicc: number
 ) => {
-	return Bodies.rectangle(
-		width / 2,
-		height,
-		width,
-		wallThickness * 2,
-		wallOptions
-	);
+  return Bodies.rectangle(width / 2, height, width, thicc * 2, wallOptions);
 };
 
 export const getLeftWall = (
-	height: number,
-	Bodies: any,
-	wallOptions: object
+  height: number,
+  Bodies: any,
+  wallOptions: object,
+  thicc: number
 ) => {
-	return Bodies.rectangle(
-		wallThickness / 2,
-		height / 2,
-		wallThickness,
-		height,
-		wallOptions
-	);
+  return Bodies.rectangle(thicc / 2, height / 2, thicc, height, wallOptions);
 };
 
 export const getRightWall = (
-	height: number,
-	width: number,
-	Bodies: any,
-	wallOptions: object
+  height: number,
+  width: number,
+  Bodies: any,
+  wallOptions: object,
+  thicc: number
 ) => {
-	return Bodies.rectangle(
-		width - wallThickness / 2,
-		height / 2,
-		wallThickness,
-		height,
-		wallOptions
-	);
+  return Bodies.rectangle(
+    width - thicc / 2,
+    height / 2,
+    thicc,
+    height,
+    wallOptions
+  );
 };
 
-const paddleThikness = 15;
-
 export const getTopPaddle = (
-	width: number,
-	wallOptions: object,
-	paddleSize: number
+  width: number,
+  wallOptions: object,
+  paddleSize: number,
+  Thiccness: number,
+  PaddleY: number
 ) => {
-	return Matter.Bodies.rectangle(
-		width / 2,
-		paddleThikness * 2,
-		paddleSize,
-		paddleThikness,
-		wallOptions
-	);
+  return Matter.Bodies.rectangle(
+    width / 2,
+    PaddleY,
+    paddleSize,
+    Thiccness,
+    wallOptions
+  );
 };
 
 export const getBottomPaddle = (
-	width: number,
-	height: number,
-	wallOptions: object,
-	paddleSize: number
+  width: number,
+  height: number,
+  wallOptions: object,
+  paddleSize: number,
+  Thiccness: number,
+  PaddleY: number
 ) => {
-	return Matter.Bodies.rectangle(
-		width / 2,
-		height - paddleThikness * 2,
-		paddleSize,
-		paddleThikness,
-		wallOptions
-	);
+  return Matter.Bodies.rectangle(
+    width / 2,
+    height - PaddleY,
+    paddleSize,
+    Thiccness,
+    wallOptions
+  );
 };
 
-export const getBall = (width: number, height: number, wallOptions: object) => {
-	return Matter.Bodies.circle(width / 2, height / 2, 14, wallOptions);
+export const getBall = (
+  width: number,
+  height: number,
+  scale: number,
+  wallOptions: object
+) => {
+  return Matter.Bodies.circle(width / 2, height / 2, scale * 15, wallOptions);
 };
