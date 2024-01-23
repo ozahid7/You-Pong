ae593ac7c735fe3b3a4c698e8d4d5c0f095f4e3e
Abderrachid Yassir <ayassir@e1r9p6.1337.ma>
2024-01-23 14:26:58 +0100

    How to play done

--- FrontEnd/components/tools/HowToPlay.tsx
+++ FrontEnd/components/tools/HowToPlay.tsx
@@ -4,7 +4,11 @@ import game from "../../public/game_sidebar.png";
 import new_game from "../../public/newgame.png";
 import Image from "next/image";
 import { TbLogicOr } from "react-icons/tb";
-import settings from "../../public/gameSettings.png"
+import settings from "../../public/settings.png";
+import { LuMouse } from "react-icons/lu";
+import black from "../../public/black.png";
+import green from "../../public/green.png";
+import orange from "../../public/orange.png";
 
 const HowToPlay = ({ isOpen, setIsOpen, closeModal }) => {
 
@@ -37,17 +41,33 @@ const HowToPlay = ({ isOpen, setIsOpen, closeModal }) => {
 							<p className="font-nunito text-[26px] break-words">There's only two ways you can create a match with another user, the <span className="text-palette-orange hover:text-palette-green">First</span> one is clicking on :</p>
 						</div>
 						<div className="flex flex-row gap-5 items-center w-[100%] justify-evenly">
-							<Image src={game} width={200} height={200} className="" alt="game" />
+							<Image src={game} width={200} height={200} className="w-[30%]" alt="game" />
 							<p className="font-nunito text-[40px] font-[500] text-palette-orange hover:text-palette-green"><TbLogicOr /></p>
-							<Image src={new_game} width={250} height={100} className="h-[100px] flex" alt="game" />
+							<Image src={new_game} width={250} height={100} className="w-[40%] h-[4%] flex" alt="game" />
 						</div>
 						<div className="flex flex-row items-center justify-between ">
-							<p className="font-number text-[30px] text-palette-orange w-[90px] self-start">2 - </p>
-							<p className="font-nunito text-[26px] break-words">After clicking on one of the buttons, you will be redirected to the <span className="text-palette-orange hover:text-palette-green">Game Options</span>, choose your options carefully, then click <span className="text-palette-orange hover:text-palette-green font-body">PLAY</span></p>
+							<p className="font-number text-[30px] text-palette-orange w-[130px] self-start">2 - </p>
+							<p className="font-nunito text-[26px] break-words">After clicking on one of the buttons, you will be redirected to the <span className="text-palette-orange hover:text-palette-green">Game Options</span>, choose your options carefully, then click <span className="text-palette-orange hover:text-palette-green font-body">Play.</span></p>
 						</div>
-						<div className="flex w-[100%] items-center justify-center debug">
+						<div className="flex w-[100%] items-center justify-center ">
 							<Image src={settings} width={300} height={300} className="w-[90%] h-full " alt="game" />
 						</div>
+						<div className="flex flex-row items-center justify-between">
+							<p className="font-number text-[30px] text-palette-orange w-[170px] self-start">3 - </p>
+							<p className="font-nunito text-[26px] break-words">After that, you will be redirected to the game page, the controls are so easy, you just have to move your mouse <span style={{ display: 'inline-flex', alignItems: 'center' }}><LuMouse className="text-palette-orange hover:text-palette-green" /></span> left or right, and try to keep the ball inside the field.</p>
+						</div>
+						<div className="flex flex-row items-center justify-between">
+							<p className="font-nunito text-[26px]">This is how the game looks like :</p>
+						</div>
+						<div className="flex flex-row items-center justify-evenly">
+							<Image src={black} width={200} height={100} className="w-[30%]" alt="black" />
+							<Image src={orange} width={200} height={100} className="w-[30%]" alt="orange" />
+							<Image src={green} width={200} height={100} className="w-[30%]" alt="green" />
+						</div>
+						<div className="flex flex-row items-center justify-between">
+							<p className="font-nunito text-[26px]">That's it, easy isn't it?</p>
+						</div>
+						<p className="font-nunito text-[30px] text-palette-orange hover:text-palette-green self-center">Good luck, Have fun</p>
 					</div>
 				</div>
 			</Fragment>
